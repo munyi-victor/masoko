5d68ae384592846d35554257465f7da5600c5a19
munyi-victor <munyivictor6@gmail.com>
2024-03-17 20:51:00 +0300

    made small screen view for product details

--- masoko-client/src/views/ProductDetailsView.vue
+++ masoko-client/src/views/ProductDetailsView.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref, onMounted } from "vue";
+import { ref, onMounted, onBeforeUnmount } from "vue";
 import { useRoute } from "vue-router";
 import axios from "axios";
 import router from "@/router";
@@ -7,17 +7,18 @@ import router from "@/router";
 const route = useRoute();
 const productId = ref(route.params.productId);
 const product = ref({});
+const screnSmall = ref(false)
 
 const fetchProducts = async () => {
   try {
-    const response = await axios.get(`http://localhost:3000/products/${productId.value}`);
+    const response = await axios.get(
+      `http://localhost:3000/products/${productId.value}`
+    );
     product.value = response.data;
   } catch (error) {
-    console.log('Error fetching products ', error);
+    console.log("Error fetching products ", error);
   }
-}
-
-onMounted(fetchProducts);
+};
 
 const addToCart = async (product) => {
   try {
@@ -26,26 +27,74 @@ const addToCart = async (product) => {
   } catch (error) {
     console.error("Error adding to cart: ", error);
   }
+};
+
+const checkScreenSize = () => {
+  screnSmall.value = window.innerWidth <= 760;
 }
+
+onMounted(() => {
+  fetchProducts();
+
+  window.addEventListener('resize', checkScreenSize);
+
+  checkScreenSize();
+});
+
+onBeforeUnmount(() => {
+  window.removeEventListener('resize', checkScreenSize);
+});
 </script>
 
 <template>
   <div>
     <div class="container-fluid">
       <div class="card mt-4 p-4">
-        <div class="d-flex gap-1">
-          <img :src="product.image_src" class="card-img-top" alt="Product image"  height="400px" />
-          <img :src="product.image_src" class="card-img-top" alt="Product image"  height="400px" />
-          <img :src="product.image_src" class="card-img-top" alt="Product image"  height="400px" />
+        <div class="d-flex justify-content-center">
+          <div v-if="screnSmall" class="d-flex justify-content-center">
+            <img
+            :src="product.image_src"
+            class="card-img-top"
+            alt="Product image"
+            height="400px"
+          />
+          </div>
+          <div class="d-flex gap-1 justify-content-center" v-else>
+            <img
+            :src="product.image_src"
+            class="card-img-top"
+            alt="Product image"
+            height="400px"
+          />
+          <img
+            :src="product.image_src"
+            class="card-img-top"
+            alt="Product image"
+            height="400px"
+          />
+          <img
+            :src="product.image_src"
+            class="card-img-top"
+            alt="Product image"
+            height="400px"
+          />
+          </div>
         </div>
 
         <div class="card-body">
           <h3 class="card-title">{{ product.name }}</h3>
-          <p class="card-text" style="font-weight: bold;">Ksh. {{ product.price }}</p>
+          <p class="card-text" style="font-weight: bold">
+            Ksh. {{ product.price }}
+          </p>
           <p class="card-text">{{ product.more_details }}</p>
 
           <div class="d-flex gap-2">
-            <button @click="addToCart(product)" class="btn btn-primary btn-dark">Add to Cart</button>
+            <button
+              @click="addToCart(product)"
+              class="btn btn-primary btn-dark"
+            >
+              Add to Cart
+            </button>
             <button class="btn btn-primary btn-dark">Buy now</button>
           </div>
         </div>
--- masoko-client/src/views/ProductsView.vue
+++ masoko-client/src/views/ProductsView.vue
@@ -1,33 +1,44 @@
 <script setup>
-import { ref, onMounted } from 'vue';
-import axios from 'axios';
+import { ref, onMounted } from "vue";
+import axios from "axios";
 
 const products = ref([]);
 
-const fetchProducts = async () => {
+onMounted(async () => {
   try {
-    const response = await axios.get('http://localhost:3000/products');
+    const response = await axios.get("http://localhost:3000/products");
     products.value = response.data;
   } catch (error) {
-    console.log('Error fetching products ', error);
+    console.log("Error fetching products ", error);
   }
-}
-
-onMounted(fetchProducts);
+});
 </script>
 
 <template>
-  <div class="container-fluid d-flex-colum">    
+  <div class="container-fluid d-flex-colum">
     <div class="row">
-      <div class="col-md-4" v-for="product in products" :key="product.id">
+      <div class="col-md-4 cont-card" v-for="product in products" :key="product.id">
         <div class="card shadow mt-4 mb-4 px-3 py-2">
-          <img :src="product.image_src" alt="HP Elitebook 840 g5 image" class="card-img-top" height="300px"/>
+          <img
+            :src="product.image_src"
+            :alt="product.name + 'Image'"
+            class="card-img-top"
+            height="300px"
+          />
           <div class="card-body">
             <h4 class="card-title">{{ product.name }}</h4>
-            <p class="card-text" style="font-weight: bold;">Ksh. {{ product.price }}</p>
-            <p class="card-text" style="margin-top: -10px;">{{ product.description }}</p>
+            <p class="card-text" style="font-weight: bold">
+              Ksh. {{ product.price }}
+            </p>
+            <p class="card-text" style="margin-top: -10px">
+              {{ product.description }}
+            </p>
             <div class="d-flex gap-2">
-              <RouterLink :to="'/' + product.id"><button class="btn btn-primary">View Product</button></RouterLink>
+              <RouterLink :to="'/' + product.id"
+                ><button class="btn btn-primary">
+                  View Product
+                </button></RouterLink
+              >
               <button class="btn btn-primary btn-dark">Buy now</button>
             </div>
           </div>
